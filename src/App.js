abc08b6d6feabd0277386b72e883550e0c065737
Arpita <arpitasanyal914@gmail.com>
2026-04-05 15:47:04 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+
+import Navbar from "./components/Layout/Navbar";
+import RoleToggle from "./components/RoleToggle/RoleToggle";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Transactions from "./components/Transactions/Transactions";
+import Insights from "./components/Insights/Insights";
+
+import { transactions as initialData } from "./data/transactions";
 
 function App() {
+  const [role, setRole] = useState("viewer");
+
+  const [transactions, setTransactions] = useState(() => {
+    const saved = localStorage.getItem("transactions");
+    return saved ? JSON.parse(saved) : initialData;
+  });
+
+  const [darkMode, setDarkMode] = useState(true);
+
+  // ✅ PASTE HERE
+  useEffect(() => {
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+  }, [transactions]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={darkMode ? "app-container dark" : "app-container"}>
+  <Navbar 
+  darkMode={darkMode}
+  setDarkMode={setDarkMode}
+  role={role}
+  setRole={setRole}
+/>
+      
+
+      <Dashboard transactions={transactions} darkMode={darkMode} />
+     <Transactions 
+  transactions={transactions} 
+  role={role} 
+  setTransactions={setTransactions} 
+/>
+      <Insights transactions={transactions} />
+
+      
     </div>
   );
 }
